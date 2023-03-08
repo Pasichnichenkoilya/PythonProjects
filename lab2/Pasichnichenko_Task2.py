cbb5dd94106a33b48dc03cc5ec5ce1374bcb3903
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-03-08 10:56:59 +0200

    Complete task2 types, user input

---
+++ lab2/Pasichnichenko_Task2.py
@@ -0,0 +1,11 @@
+# Multi-paradigm programming languages Lab2 Illia Pasichnichenko IKM-221a
+print('Multi-paradigm programming languages Lab2')
+print('Illia Pasichnichenko IKM-221a')
+
+x = float(input("Enter x value: "))
+y = float(input("Enter y value: "))
+z = float(input("Enter z value: "))
+
+result = (11 + 2 * x + 4.1) / (12.4 - y) + z
+
+print(result)
