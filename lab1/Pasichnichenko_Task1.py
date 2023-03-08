ee085868aa80c22735fcaa1b2eae86932ef56699
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-03-08 20:21:39 +0200

    fix task_1: name output

--- lab1/Pasichnichenko_Task1.py
+++ lab1/Pasichnichenko_Task1.py
@@ -1,12 +1,11 @@
 # 3.1
-print("'Multi-paradigm programming languages':Task 1")
-print("Pasichnichenko Illia IKM-221a")
+print('Multi-paradigm programming languages Task 1')
+print('Illia Pasichnichenko IKM-221a')
 
 # 3.2
-for i in range(45):
-    separator = ', ' if i < 44 else ''
-    print(f'Vladyslav Valeriyovych Ovsyannikov{separator}', end='')
+names = ['Vladyslav Valeriyovych Ovsyannikov'] * 45
+print(', '.join(names))
 
 # 3.2
 result = (11 + 2 * 3 + 4.1) / (12.4 - 221.3) + 4.8
-print(f'\n{result}')
+print(result)
