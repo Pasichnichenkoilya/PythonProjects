4d92b9660309360bb3345d121715e3bed29cbf73
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-03-08 12:16:42 +0200

    Refactor names print

--- lab1/Pasichnichenko_Task1.py
+++ lab1/Pasichnichenko_Task1.py
@@ -1,14 +1,10 @@
 # 3.1
-cource_task_name = 'Multi-paradigm programming languages', 'Task 1'
-print(':'.join(cource_task_name))
-
-student_info = {'name': 'Illia', 'surname': 'Pasichnichenko', 'group': 'IKM-221a'}
-print(' '.join(student_info.values()))
+print('Multi-paradigm programming languages Task 1')
+print('Illia Pasichnichenko IKM-221a')
 
 # 3.2
-for i in range(45):
-    separator = ', ' if i < 44 else '\n'
-    print('Vladyslav Valeriyovych Ovsyannikov', end=separator)
+names = ['Vladyslav Valeriyovych Ovsyannikov'] * 45
+print(', '.join(names))
 
 # 3.2
 result = (11 + 2 * 3 + 4.1) / (12.4 - 221.3) + 4.8
