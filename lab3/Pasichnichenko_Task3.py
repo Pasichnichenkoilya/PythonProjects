a3a4df0a2f2b390174b604affb17fc809dcb95d4
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-03-11 11:02:55 +0200

    refactor task_3

--- lab3/Pasichnichenko_Task3.py
+++ lab3/Pasichnichenko_Task3.py
@@ -1,30 +1,34 @@
 from math import sin, atan
 
 
-def get_float(message):
+def get_float(var_name, default_message='Enter ', end_str=': '):
     try:
-        user_input = float(input(message))
-        if isinstance(user_input, float):
-            return user_input
+        user_input = float(input(default_message + var_name + end_str))
+        return user_input
     except:
         print('It must be a number')
-        return get_float(message)
+    return get_float(var_name, default_message, end_str)
 
 
-def get_not_equal_to_0_number(number_to_validate):
-    while number_to_validate == 0:
-        number_to_validate = get_float('the number cannot be equal to 0, try again: ')
-    return float(number_to_validate)
+def validate(value, condition_lambda, error_msg):
+    while condition_lambda(value):
+        value = get_float(error_msg, '')
+    return value
+
+
+def equal_to_zero(value):
+    return value == 0
 
 
 # Multi-paradigm programming languages Lab2 Illia Pasichnichenko IKM-221a
 print('Multi-paradigm programming languages Lab2')
 print('Illia Pasichnichenko IKM-221a')
 
-a = get_not_equal_to_0_number(get_float('Enter a value: '))
-b = get_not_equal_to_0_number(get_float('Enter b value: '))
-c = get_not_equal_to_0_number(get_float('Enter c value: '))
+equal_to_zero_msg = 'the value cannot be equal to zero'
+
+a = validate(get_float('a'), equal_to_zero, equal_to_zero_msg)
+b = validate(get_float('b'), equal_to_zero, equal_to_zero_msg)
+c = validate(get_float('c'), equal_to_zero, equal_to_zero_msg)
 
 y = (sin(2 * a) / a - 3) + (atan(b) / c)
-
 print(y)
