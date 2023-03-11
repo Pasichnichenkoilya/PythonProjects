78c59902c3a9b0a405cdfc98b42a8d04f2ffac05
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-03-11 10:54:09 +0200

    refactor task_2

--- lab2/Pasichnichenko_Task2.py
+++ lab2/Pasichnichenko_Task2.py
@@ -3,20 +3,24 @@ print('Multi-paradigm programming languages Lab2')
 print('Illia Pasichnichenko IKM-221a')
 
 
-def get_float(message):
+def get_float(var_name, default_message='Enter ', end_str=': '):
     try:
-        user_input = float(input(message))
-        if isinstance(user_input, float):
-            return user_input
+        user_input = float(input(default_message + var_name + end_str))
+        return user_input
     except:
         print('It must be a number')
-        return get_float(message)
+    return get_float(var_name, default_message, end_str)
 
 
-x = get_float('Enter x: ')
-y = get_float('Enter y: ')
-z = get_float('Enter z: ')
+def validate(value, condition_lambda, error_msg):
+    while condition_lambda(value):
+        value = get_float(error_msg)
+    return value
+
+
+x = get_float('x')
+y = validate(get_float('y'), lambda _: _ == 12.4, 'the value cannot be equal to 12.4, enter again: ')
+z = get_float('z')
 
 result = (11 + 2 * x + 4.1) / (12.4 - y) + z
-
 print(result)
