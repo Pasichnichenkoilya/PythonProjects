d1a47bfcfe62cca15298ad75ac8d614c45525c8a
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-03-15 21:14:20 +0200

    move function to the top

--- lab4/Pasichnichenko_Task4.py
+++ lab4/Pasichnichenko_Task4.py
@@ -1,17 +1,17 @@
 from utils import get_number
 from functools import reduce
 
-print('Multi-paradigm programming languages Lab 4')
-print('Illia Pasichnichenko IKM-221a')
-
-number = get_number('an integer', int)
-
 
 def add(current_sum, current_number):
     return current_sum + current_number / (current_number + 1)
 
 
-odd_numbers =  range(1, number * 2, 2)
+print('Multi-paradigm programming languages Lab 4')
+print('Illia Pasichnichenko IKM-221a')
+
+number = get_number('an integer', int)
+
+odd_numbers = range(1, number * 2, 2)
 result = reduce(add, odd_numbers, 0)
 
 print('Result: ', result)
