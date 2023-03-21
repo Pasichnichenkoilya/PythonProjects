897a0de53dee1158dc0b8bd88ab652e07c8ee6c1
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-03-21 09:34:46 +0200

    refactor calculations

--- lab5/Pasichnichenko_Task5_1.py
+++ lab5/Pasichnichenko_Task5_1.py
@@ -1,17 +1,12 @@
 from math import factorial
+from itertools import takewhile, count, repeat
 
 # Multi-paradigm programming languages Lab_5.1 Illia Pasichnichenko IKM-221a
 print('Multi-paradigm programming languages Lab 5.1')
 print('Illia Pasichnichenko IKM-221a')
 
-epsilon = 10 ** - 4
-series_sum = 0
 n = 1
-a = 1
+row = (10 ** -n * factorial(n - 1) for n in count(n))
+row_sum = sum(list(takewhile(lambda x: x >= 1e-4, row)))
 
-while abs(a) >= epsilon:
-    a = 10 ** -n * factorial(n - 1)
-    series_sum += a
-    n += 1
-
-print(series_sum)
+print(row_sum)
