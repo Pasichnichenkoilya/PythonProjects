4bda67f72614358e5f02c336adc88a803c01e4c4
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-04-02 03:14:03 +0300

    Add crappy solution

--- lab7/tests/unit/test_line.py
+++ lab7/tests/unit/test_line.py
@@ -1,5 +1,38 @@
-from lab7.line import Line
-from lab7.point import Point
+import math as m
+
+
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'Point(x: {self.x}, y: {self.y})'
+
+
+class Line:
+    def __init__(self, point_a: Point, point_b: Point):
+        self.point_a = point_a
+        self.point_b = point_b
+
+    def __str__(self):
+        return f'Line: [{self.point_a}, {self.point_b}]'
+
+    def get_length(self):
+        return m.sqrt((self.point_b.x - self.point_a.x) ** 2 +
+                      (self.point_b.y - self.point_a.y) ** 2)
+
+    def get_middle_point(self):
+        x = (self.point_a.x + self.point_b.x) / 2
+        y = (self.point_a.y + self.point_b.y) / 2
+
+        return Point(x, y)
+
+    def get_length_projection_x(self):
+        return abs(self.point_b.x - self.point_a.x)
+
+    def get_length_projection_y(self):
+        return abs(self.point_b.y - self.point_a.y)
 
 
 def test_get_length():
--- lab7/tests/unit/test_point.py
+++ lab7/tests/unit/test_point.py
@@ -1,4 +1,10 @@
-from lab7.point import Point
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'Point(x: {self.x}, y: {self.y})'
 
 
 def test_str():
